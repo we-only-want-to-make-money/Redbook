7cbdee8bd03c6a497cee6ddcdb5619b20685a3f2
hongch <hongch@ufotosoft.com>
2019-08-07 18:06:01 +0800

    T

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -61,7 +61,8 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        echo "SiteController";
+        //return $this->render('index');
     }
 
     /**
