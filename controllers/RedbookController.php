49698fedf725df76bcebe5207c17f2659924c4dc
hongch <hongch@ufotosoft.com>
2019-08-08 10:29:53 +0800

    t

--- controllers/RedbookController.php
+++ controllers/RedbookController.php
@@ -23,7 +23,8 @@ class RedbookController  extends Controller
     {
 
         $ch = curl_init();
-        $url="https://www.xiaohongshu.com/discovery/item/5d494d7a000000002801e154?xhsshare=CopyLink&appuid=5c0a053e000000000500b9f2&apptime=1565158031";
+        //$url="https://www.xiaohongshu.com/discovery/item/5d494d7a000000002801e154?xhsshare=CopyLink&appuid=5c0a053e000000000500b9f2&apptime=1565158031";
+        $url="http://www.shcydy.com";
         curl_setopt ( $ch , CURLOPT_USERAGENT ,"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.113 Safari/537.36" );
         curl_setopt($ch,CURLOPT_URL,$url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -38,3 +39,4 @@ class RedbookController  extends Controller
        return 'success';
     }
 }
+
